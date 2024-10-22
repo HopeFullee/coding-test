5241b829b77adb423c0bce47829b79b1eda2585a
HopeFullee <john_lee0607@naver.com>
2024-10-23 00:34:16 +0900

    chore: JS Queue 생성자 사용 예시 작성

--- fast-campus/ch-02-data-structure/04-js-queue/index.js
+++ fast-campus/ch-02-data-structure/04-js-queue/index.js
@@ -23,3 +23,22 @@ class Queue {
     return this.items.tailIndex - this.headIndex;
   }
 }
+
+// 구현된 큐(Queue) 생성자 사용
+const queue = new Queue();
+
+// 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7)
+// - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+queue.enqueue(5);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(7);
+queue.dequeue();
+queue.enqueue(1);
+queue.enqueue(4);
+queue.dequeue();
+
+// 먼저 들어온 순서대로 삭제 및 출력
+while (queue.getLength() !== 0) {
+  console.log(queue.dequeue());
+}
