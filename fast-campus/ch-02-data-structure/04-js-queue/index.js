379cafe042c1e557ded85ab1a6ad04688cbe179e
HopeFullee <john_lee0607@naver.com>
2024-10-23 00:27:13 +0900

    chore: JS Queue 구현

---
+++ fast-campus/ch-02-data-structure/04-js-queue/index.js
@@ -0,0 +1,25 @@
+// javascript 큐(Queue) 구현하기
+
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue() {
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.headIndex];
+  }
+  getLength() {
+    return this.items.tailIndex - this.headIndex;
+  }
+}
