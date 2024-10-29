701e8d5d80776b772cde59636ad7f67fe5179168
HopeFullee <john_lee0607@naver.com>
2024-10-30 06:00:38 +0900

    chore: 문자열 병합 정렬 merge sort 라이브러리 사용 예시 학습

--- fast-campus/ch-03-sorting/05-sort-library/index.js
+++ fast-campus/ch-03-sorting/05-sort-library/index.js
@@ -1,16 +1,19 @@
 // merge sort 병합 정렬 라이브러리 사용 예시
-const arr = [1, 8, 5, 9, 21, 3, 7, 2, 15];
 
-const compare = (a, b) => {
+// 정수(Number)에 대한 정렬 예시
+const numArr = [1, 8, 5, 9, 21, 3, 7, 2, 15];
+
+// ex) 오름차 예시 / 내림차 경우 아래 (a)와 (b)의 계산 <=> 위치 변경
+const NumCompare = (a, b) => {
   if (a < b) return -1;
   else if (a > b) return 1;
   else return 0;
 };
 
-arr.sort(compare);
+numArr.sort(NumCompare);
 
-// 축약 버전
-arr.sort((a, b) => {
+// 축약 버전 오름차 / 내림차 경우 b - a
+numArr.sort((a, b) => {
   return a - b;
 });
 
@@ -40,3 +43,41 @@ arr.sort((a, b) => {
      - (0)이 리턴된다면
        1. 파라미터(a, b)의 순서를 변경하지 않는다.
 */
+
+/**************************************************************************************************/
+
+// 문자열(String)에 대한 정렬 예시
+const strArr = ["pineapple", "banana", "durian", "apple", "carrot"];
+
+// 문자열 오름차순 정렬 예시
+// 별도로 비교 함수(compare function)을 사용하지 않으면, 유니코드 순으로 정렬된다.
+strArr.sort();
+
+// 실행 결과 ['apple', 'banana', 'carrot', 'durian', 'pineapple'] (오름차)
+console.log(strArr);
+
+// 문자열 내림차순 정렬 예시
+const strCompare = (a, b) => {
+  if (b < a) return -1;
+  else if (b > a) return 1;
+  else return 0;
+};
+
+// 문자열(String)에 대한 정렬 예시 (대소문자 구분X)
+const capitalMixStrArr = ["pineapple", "Banana", "durian", "Apple", "carrot"];
+
+// 문자열 오름차순 정렬 예시 (대소문자 구분X)
+// 대소문자 구분 없는 내림차는 caseA 와 caseB의 계산 <=> 위치 변경
+const capitalMixStrComapre = (a, b) => {
+  const upperCaseA = a.toUpperCase();
+  const upperCaseB = b.toUpperCase();
+
+  if (upperCaseA < upperCaseB) return -1;
+  else if (upperCaseA > upperCaseB) return 1;
+  else return 0;
+};
+
+capitalMixStrArr.sort(capitalMixStrComapre);
+
+// 실행 결과 ['Apple', 'Banana', 'carrot', 'durian', 'pineapple'] (대소문자 구분X)
+console.log(capitalMixStrArr);
