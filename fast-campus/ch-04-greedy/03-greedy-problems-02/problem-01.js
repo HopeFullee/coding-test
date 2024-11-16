5a8ebe05c85d1fd6c141b44a579dc42b9eaa096e
HopeFullee <john_lee0607@naver.com>
2024-11-16 19:56:34 +0900

    chore: 2839 solved

---
+++ fast-campus/ch-04-greedy/03-greedy-problems-02/problem-01.js
@@ -0,0 +1,37 @@
+// https://www.acmicpc.net/problem/2839
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+let n = Number(input);
+
+let cnt = 0;
+
+// 자작 답안
+if (n % 5 === 0) {
+  cnt = n / 5;
+} else {
+  while (n % 5 !== 0 && n > 0) {
+    n -= 3;
+    cnt++;
+  }
+
+  if (n >= 0) cnt += n / 5;
+  else cnt = -1;
+}
+
+console.log(cnt);
+
+// 강의 답안
+while (n >= 0) {
+  if (n == 0 || n % 5 == 0) {
+    cnt += n / 5;
+    console.log(cnt);
+    flag = true;
+    break;
+  }
+
+  n -= 3;
+  cnt += 1;
+}
+
+if (!flag) console.log(-1);
