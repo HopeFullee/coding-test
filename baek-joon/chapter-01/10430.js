69985445f07b42b9faab3637fd0e37d83bda9d0b
HopeFullee <john_lee0607@naver.com>
2024-12-25 23:42:30 +0900

    chore: baekjoon 10430 solved

---
+++ baek-joon/chapter-01/10430.js
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/10430
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+const [A, B, C] = input.split(" ").map(Number);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
