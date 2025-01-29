0f6f84506ac7160aa7fa3bee7383cc7c86400134
HopeFullee <john_lee0607@naver.com>
2025-01-29 21:29:26 +0900

    chore: baekjoon 11718 solved

---
+++ baek-joon/steps/step-05/11718.js
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/11718
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+input.forEach((tc) => {
+  console.log(tc);
+});
