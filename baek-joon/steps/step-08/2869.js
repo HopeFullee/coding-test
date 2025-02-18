949dbb191518e5011e1c46196cbb0741c3fc2123
HopeFullee <john_lee0607@naver.com>
2025-02-18 20:21:12 +0900

    chore: baekjoon 5086 solved

--- baek-joon/steps/step-08/2869.js
+++ baek-joon/steps/step-08/2869.js
@@ -3,8 +3,6 @@
 /*
   구현없이 순도 100% 수학 문제는 아무리 머리를 굴려봐도 풀 수 없었다 ㅠㅠ..
   출처: https://www.youtube.com/watch?v=6Eu2j_0s-kk
-
-  
 */
 
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
---
+++ baek-joon/steps/step-09/5086.js
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/11005
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+input.pop();
+
+input.forEach((line) => {
+  const [a, b] = line.split(" ").map(Number);
+
+  if (a < b) {
+    if (b % a === 0) console.log("factor");
+    else console.log("neither");
+  } else {
+    if (a % b === 0) console.log("multiple");
+    else console.log("neither");
+  }
+});
