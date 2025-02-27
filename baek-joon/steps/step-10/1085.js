458cf31190777e3f8bc1b18bb6384a3455d27b1b
HopeFullee <john_lee0607@naver.com>
2025-02-27 13:11:20 +0900

    chore: baekjoon 1085 solved

---
+++ baek-joon/steps/step-10/1085.js
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/problem/1085
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+const [x, y, w, h] = input.split(" ").map(Number);
+
+console.log(Math.min(x, y, w - x, h - y));
