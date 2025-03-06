adc4859e22b42bd6edb0a0b5161f52bc3c5c5452
HopeFullee <john_lee0607@naver.com>
2025-03-06 23:15:22 +0900

    chore: baekjoon 24262 solved

---
+++ baek-joon/steps/step-11/24262.js
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/24262
+
+/*
+  알고리즘 문제 보단 문제 자체 즉 시간복잡도를 이해하고 있나 테스트 하는 문제였다.
+
+  출처: https://kevinitcoding.tistory.com/entry/%EB%B0%B1%EC%A4%80Python-24262%EB%B2%88-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EC%88%98%EC%97%85-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EC%9D%98-%EC%88%98%ED%96%89-%EC%8B%9C%EA%B0%84-1-%EB%AC%B8%EC%A0%9C
+*/
+
+console.log(1);
+console.log(0);
