ceed3aa0e76f1ab3bd1da46031c26fc3dc35db05
HopeFullee <john_lee0607@naver.com>
2025-03-09 22:56:21 +0900

    chore: baekjoon 24264 solved

---
+++ baek-joon/steps/step-11/24264.js
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/24264
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+const n = Number(input);
+
+console.log(n * n);
+console.log(2);
