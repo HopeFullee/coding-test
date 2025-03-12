a4de013bedc0f673941857efa2e377b50510b9a9
HopeFullee <john_lee0607@naver.com>
2025-03-12 19:36:34 +0900

    refactor: baekjoon 24267 number에서 bigInt로 수정

--- baek-joon/steps/step-11/24267.js
+++ baek-joon/steps/step-11/24267.js
@@ -5,13 +5,13 @@
   원인은 시그마 풀이 법을 모르면 풀기 어려운 문제였고 본인은 수포자이다..
 
   코테 단계를 진행하며 부분 적으로 수학 공부를 하게되는 것에 의미를 두고 버티자.. 
+
+  풀이에 도움이 된 블로그 - 출처: https://minjo0n.tistory.com/13
 */
 
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
-const n = Number(input);
+const n = BigInt(input);
 
-(n * (n - 1) * (n - 2)) / 6;
-
-console.log();
+console.log(`${(n * (n - BigInt(1)) * (n - BigInt(2))) / BigInt(6)}`);
 console.log(3);
