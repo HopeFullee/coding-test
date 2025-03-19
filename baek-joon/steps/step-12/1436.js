09f94960fcac47558c1c9402584af98e1e9127e5
HopeFullee <john_lee0607@naver.com>
2025-03-19 22:57:27 +0900

    chore: baekjoon 2839 solved

--- baek-joon/steps/step-12/1436.js
+++ baek-joon/steps/step-12/1436.js
@@ -1,21 +1,18 @@
 // https://www.acmicpc.net/problem/1018
 
-// const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
-// const n = Number(input);
-
-const n = 187;
+const n = Number(input);
 
 const s = "666";
 let count = 0;
-let i = 0;
+let i = 666;
 
 while (true) {
   if (i.toString().includes(s)) {
     count++;
     if (count === n) break;
   }
-
   i++;
 }
 
---
+++ baek-joon/steps/step-12/2839.js
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/2839
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+// let n = Number(input);
+
+let count = 0;
+
+while (n > 0) {
+  if (n % 5 === 0) {
+    count += n / 5;
+    break;
+  } else {
+    n -= 3;
+    if (n < 0) count = -1;
+    else count++;
+  }
+}
+
+console.log(count);
