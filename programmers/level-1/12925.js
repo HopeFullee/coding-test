cf88d69ae8e62d327ef27055215d65df91dc03f4
HopeFullee <john_lee0607@naver.com>
2025-05-02 21:25:04 +0900

    chore: programmers 12925 solved

---
+++ programmers/level-1/12925.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/12925
+  12925 - 문자열을 정수로 바꾸기
+*/
+
+function solution(s) {
+  return Number(s);
+}
+
+const s = "-1234";
+
+console.log(solution(s));
