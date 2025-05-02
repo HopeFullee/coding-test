bf5f37238c6ce4d7857c48a00fb08785556aaaff
HopeFullee <john_lee0607@naver.com>
2025-05-02 20:55:29 +0900

    chore: programmers 12932 solved

---
+++ programmers/level-1/12932.js
@@ -0,0 +1,13 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/12932
+  12932 - 자연수 뒤집어 배열로 만들기
+*/
+
+function solution(n) {
+  const reversedNumArr = n.toString().split("").reverse().map(Number);
+  return reversedNumArr;
+}
+
+const n = 12345;
+
+console.log(solution(n));
