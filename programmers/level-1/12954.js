4e47020c94bd190e4aca6d89838de7275581fe95
HopeFullee <john_lee0607@naver.com>
2025-05-02 21:13:14 +0900

    chore: programmers 12954 solved

---
+++ programmers/level-1/12954.js
@@ -0,0 +1,19 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/12954
+  12954 - x만큼 간격이 있는 n개의 숫자
+*/
+
+function solution(x, n) {
+  const answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    answer.push(x * i);
+  }
+
+  return answer;
+}
+
+const x = 2;
+const n = 5;
+
+console.log(solution(x, n));
