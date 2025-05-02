3c1e5ea6683317c96d194eec3bfd85593ab96585
HopeFullee <john_lee0607@naver.com>
2025-05-02 21:08:43 +0900

    chore: programmers 12944 solved

---
+++ programmers/level-1/12944.js
@@ -0,0 +1,16 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/12944
+  12944 - 평균 구하기
+*/
+
+// arr에 int를 다 더하고 arr의 길이만큼 나누면 평균값이 나온다.
+
+function solution(arr) {
+  const sum = arr.reduce((acc, num) => acc + num);
+  const avg = sum / arr.length;
+  return avg;
+}
+
+const arr = [1, 2, 3, 4];
+
+console.log(solution(arr));
