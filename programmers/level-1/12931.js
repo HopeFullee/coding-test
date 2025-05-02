a916f95e016bfe5f6eab8f7211474619ffbcfa34
HopeFullee <john_lee0607@naver.com>
2025-05-02 20:52:51 +0900

    chore: programmers 12931 solved

---
+++ programmers/level-1/12931.js
@@ -0,0 +1,14 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/12931
+  12931 - 자릿수 더하기
+*/
+
+function solution(n) {
+  const numArr = n.toString().split("").map(Number);
+  const sum = numArr.reduce((acc, num) => acc + num);
+  return sum;
+}
+
+const n = 987;
+
+console.log(solution(n));
