3789e50d77b8d263ab512159dfca41049e14e6a5
HopeFullee <john_lee0607@naver.com>
2025-05-10 21:54:07 +0900

    chore: programmers 12918 solved

---
+++ programmers/level-1/practice/12918.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/12918
+  연습문제 - 문자열 다루기 기본
+*/
+
+function solution(s) {
+  return s.length === 4 || s.length === 6 ? !isNaN(s) : false;
+}
+
+const s = "1234";
+
+console.log(solution(s));
