f297b8e37c1057bcf07fb591a770e58b0b8e885b
HopeFullee <john_lee0607@naver.com>
2025-05-22 04:07:13 +0900

    chore: programmers 12977 solved

--- programmers/level-1/summer-winter-2018/12977.js
+++ programmers/level-1/summer-winter-2018/12977.js
@@ -2,3 +2,34 @@
   https://school.programmers.co.kr/learn/courses/30/lessons/12977
   Summer/Winter Coding(~2018) - 소수 만들기
 */
+
+const isPrime = (num) => {
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+function solution(nums) {
+  const sumArr = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        sumArr.push(nums[i] + nums[j] + nums[k]);
+      }
+    }
+  }
+
+  let primeCount = 0;
+
+  for (const num of sumArr) {
+    if (isPrime(num)) primeCount++;
+  }
+
+  return primeCount;
+}
+
+const nums = [1, 2, 7, 6, 4];
+
+console.log(solution(nums));
