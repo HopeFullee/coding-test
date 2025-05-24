5227f56c987195337719b0c5cffb304ed3d83f67
HopeFullee <john_lee0607@naver.com>
2025-05-25 00:00:09 +0900

    refactor: programmers 140108 refactored

--- programmers/level-1/practice/140108.js
+++ programmers/level-1/practice/140108.js
@@ -33,3 +33,26 @@ function solution(s) {
 const s = "banana";
 
 console.log(solution(s));
+
+/*
+  [모범 답안]
+*/
+
+function solution2(s) {
+  let answer = 0;
+  let current;
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (count === 0) {
+      answer++;
+      current = s[i];
+      count = 1;
+    } else {
+      if (current !== s[i]) count--;
+      else count++;
+    }
+  }
+
+  return answer;
+}
