a3a02122be2613af7e702dcf8a67d113ca9b7923
HopeFullee <john_lee0607@naver.com>
2025-05-27 23:27:18 +0900

    chore: programmers 120898 solved

---
+++ programmers/level-0/intro/120898.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/120898
+  코딩테스트 입문 - 편지
+*/
+
+function solution(message) {
+  return message.length * 2;
+}
+
+const message = "happy birthday!";
+
+console.log(solution(message));
