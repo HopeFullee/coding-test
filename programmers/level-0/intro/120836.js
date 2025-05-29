1ffa6c8534d4b382fb2fc9bf6961c6b1590904fa
HopeFullee <john_lee0607@naver.com>
2025-05-30 00:22:32 +0900

    chore: programmers 120836 solved

---
+++ programmers/level-0/intro/120836.js
@@ -0,0 +1,19 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/120836
+  코딩테스트 입문 - 순서쌍의 개수
+*/
+
+function solution(n) {
+  let count = 0;
+
+  for (let i = 1; i * i <= n; i++) {
+    if (i * i === n) count++;
+    else if (n % i === 0) count += 2;
+  }
+
+  return count;
+}
+
+const n = 20;
+
+console.log(solution(n));
