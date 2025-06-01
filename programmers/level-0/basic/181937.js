e945153d2d06effd4c70c3b56da772444c7c6d6f
HopeFullee <john_lee0607@naver.com>
2025-06-01 17:40:45 +0900

    chore: programmers 181937 solved

---
+++ programmers/level-0/basic/181937.js
@@ -0,0 +1,11 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181937
+  코딩 기초 트레이닝 - n의 배수
+*/
+
+function solution(num, n) {
+  return num % n === 0 ? 1 : 0;
+}
+
+const num = 98;
+const n = 2;
