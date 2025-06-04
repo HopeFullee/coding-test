87462b1acc7a1f4832c6d706e39920a949004775
HopeFullee <john_lee0607@naver.com>
2025-06-04 23:34:09 +0900

    chore: programmers 181941 solved

---
+++ programmers/level-0/basic/181941.js
@@ -0,0 +1,10 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181941
+  코딩 기초 트레이닝 - 문자 리스트를 문자열로 변환하기
+*/
+
+function solution(arr) {
+  return arr.join("");
+}
+
+const arr = ["a", "b", "c"];
