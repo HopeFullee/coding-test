37ca5d86537a61ceabaade31072de14b4c963883
HopeFullee <john_lee0607@naver.com>
2025-06-09 21:10:31 +0900

    chore: programmers 181877 solved

---
+++ programmers/level-0/basic/181877.js
@@ -0,0 +1,10 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181877
+  코딩 기초 트레이닝 - 대문자로 바꾸기
+*/
+
+function solution(myString) {
+  return myString.toUpperCase();
+}
+
+const myString = "aBcDeFg";
