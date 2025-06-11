5841c983e5b987cbaaa24e62648baecf4ffa927d
HopeFullee <john_lee0607@naver.com>
2025-06-11 23:34:47 +0900

    chore: programmers 181876 solved

---
+++ programmers/level-0/basic/181876.js
@@ -0,0 +1,10 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181876
+  코딩 기초 트레이닝 - 소문자로 바꾸기
+*/
+
+function solution(myString) {
+  return myString.toLowerCase();
+}
+
+const myString = "aBcDeFg";
