9b9c2d6d8c84208818a8686a927382a3c8d68bad
HopeFullee <john_lee0607@naver.com>
2025-06-11 23:53:45 +0900

    chore: programmers 181882 solved

---
+++ programmers/level-0/basic/181882.js
@@ -0,0 +1,18 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181882
+  코딩 기초 트레이닝 - 조건에 맞게 수열 변환하기 1
+*/
+
+function solution(arr) {
+  return arr.map((n) => {
+    if (n > 49 && n % 2 === 0) return n / 2;
+
+    if (n < 50 && n % 2) return n * 2;
+
+    return n;
+  });
+}
+
+const arr = [1, 2, 3, 100, 99, 98];
+
+console.log(solution(arr));
