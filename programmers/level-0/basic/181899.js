6a4326a7b33e6c7bd9b51c6fe45f5ceaf00ba75f
HopeFullee <john_lee0607@naver.com>
2025-06-14 00:05:17 +0900

    chore: programmers 181899 solved

--- programmers/level-0/basic/181899.js
+++ programmers/level-0/basic/181899.js
@@ -13,7 +13,12 @@ function solution(start_num, end_num) {
   return ans;
 }
 
+function solution2(start_num, end_num) {
+  return [...Array(start_num - end_num + 1)].map((_, idx) => start_num - idx);
+}
+
 const start_num = 10;
 const end_num = 3;
 
 console.log(solution(start_num, end_num));
+console.log(solution2(start_num, end_num));
