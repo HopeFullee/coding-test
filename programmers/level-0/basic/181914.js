323ea9ea3dd39ee3b06aeab1899241f4af85db9e
HopeFullee <john_lee0607@naver.com>
2025-06-14 23:59:42 +0900

    chore: programmers 181914 solved

---
+++ programmers/level-0/basic/181914.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181914
+  코딩 기초 트레이닝 - 9로 나눈 나머지
+*/
+
+function solution(number) {
+  const sum = [...number].reduce((a, ch) => a + Number(ch), 0);
+  return sum % 9;
+}
+
+const number = "78720646226947352489";
+console.log(solution(number));
