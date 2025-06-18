ccae2226e205a9b929a9766d4e8e6e0bf63b981d
HopeFullee <john_lee0607@naver.com>
2025-06-18 23:21:27 +0900

    chore: programmers 181930 solved

---
+++ programmers/level-0/basic/181930.js
@@ -0,0 +1,21 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181930
+  코딩 기초 트레이닝 - 주사위 게임 2
+*/
+
+function solution(a, b, c) {
+  let sumArr = [];
+  const dupe = new Set([a, b, c]);
+
+  for (let i = 1; i <= 4 - dupe.size; i++) {
+    sumArr.push(a ** i + b ** i + c ** i);
+  }
+
+  return sumArr.reduce((a, sum) => a * sum);
+}
+
+const a = 2;
+const b = 6;
+const c = 1;
+
+console.log(solution(a, b, c));
