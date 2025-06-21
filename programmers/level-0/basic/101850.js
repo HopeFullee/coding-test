b8d704fa2cc10e6e2d7ce04875b76b873deb7fcd
HopeFullee <john_lee0607@naver.com>
2025-06-21 18:48:38 +0900

    chore: programmers 181850 solved

---
+++ programmers/level-0/basic/101850.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181850
+  코딩 기초 트레이닝 - 정수 부분
+*/
+
+function solution(flo) {
+  return parseInt(flo);
+}
+
+const flo = 1.42;
+
+console.log(solution(flo));
