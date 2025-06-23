87532db2a5f7fa71e922c349527aea10d6fde129
HopeFullee <john_lee0607@naver.com>
2025-06-23 23:24:34 +0900

    chore: programmers 181847 solved

---
+++ programmers/level-0/basic/181847.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181847
+  코딩 기초 트레이닝 - 0 떼기
+*/
+
+function solution(n_str) {
+  return Number(n_str).toString();
+}
+
+const n_str = "0010";
+
+console.log(solution(n_str));
