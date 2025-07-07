c2eb25419a77cd20895255181e55f3668b11ff77
HopeFullee <john_lee0607@naver.com>
2025-07-07 23:34:30 +0900

    chore: programmers 181848 solved

---
+++ programmers/level-0/basic/181848.js
@@ -0,0 +1,12 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181848
+  코딩 기초 트레이닝 - 문자열을 정수로 변환하기
+*/
+
+function solution(n_str) {
+  return Number(n_str);
+}
+
+const n_str = "10";
+
+console.log(solution(n_str));
