a0a19da8efa70878dbe1da3fd4eb7a37af70bcf8
HopeFullee <john_lee0607@naver.com>
2025-09-01 14:42:46 +0900

    chore: programmers 181896 solved

---
+++ programmers/level-0/basic/181896.js
@@ -0,0 +1,15 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181896
+  코딩 기초 트레이닝 - 첫 번째로 나오는 음수
+*/
+
+function solution(num_list) {
+  return num_list.indexOf((n) => {
+    n;
+    return n == 4;
+  });
+}
+
+const num_list = [12, 4, 15, 46, 38, -2, 15];
+
+console.log(solution(num_list));
