f1f9ff3d0659adf19f78a873f7c81ed1483671aa
HopeFullee <john_lee0607@naver.com>
2025-09-23 23:56:12 +0900

    chore: programmers 181917 solved

---
+++ programmers/level-0/basic/181917.js
@@ -0,0 +1,15 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181917
+  코딩 기초 트레이닝 - 간단한 논리 연산
+*/
+
+function solution(x1, x2, x3, x4) {
+  return (x1 || x2) && (x3 || x4);
+}
+
+const x1 = false;
+const x2 = true;
+const x3 = false;
+const x4 = false;
+
+console.log(solution(x1, x2, x3, x4));
