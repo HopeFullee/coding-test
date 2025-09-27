ee97e726218f2ef2da5b3e7f0e5e4fae39ac29ae
HopeFullee <john_lee0607@naver.com>
2025-09-28 01:33:25 +0900

    chore: programmers 181890 solved

---
+++ programmers/level-0/basic/181890.js
@@ -0,0 +1,24 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181890
+  코딩 기초 트레이닝 - 왼쪽 오른쪽
+*/
+
+function solution(str_list) {
+  const some = str_list.some((str, idx) => {
+    if (str === "l") {
+      str_list.splice(idx);
+      return true;
+    }
+
+    if (str === "r") {
+      str_list.splice(0, idx + 1);
+      return true;
+    }
+  });
+
+  return some ? str_list : [];
+}
+
+const str_list = ["u", "u", "l", "u"];
+
+console.log(solution(str_list));
