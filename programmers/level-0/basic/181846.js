1f5afc8b8ad3ae9946f22436e15471761b2f4901
HopeFullee <john_lee0607@naver.com>
2025-10-03 23:51:51 +0900

    chore: programmers 181846 solved

---
+++ programmers/level-0/basic/181846.js
@@ -0,0 +1,13 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/181846
+  코딩 기초 트레이닝 - 두 수의 합
+*/
+
+function solution(a, b) {
+  return (Number(a) + Number(b)).toString();
+}
+
+const a = "18446744073709551615";
+const b = "287346502836570928366";
+
+console.log(solution(a, b));
