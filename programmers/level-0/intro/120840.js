e4e221ed062131a176aada45841f0b419d2c4a1d
HopeFullee <john_lee0607@naver.com>
2025-10-13 23:39:15 +0900

    chore: programmers 120840 solved

---
+++ programmers/level-0/intro/120840.js
@@ -0,0 +1,29 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/120840
+  코딩테스트 입문 - 구슬을 나누는 경우의 수
+*/
+
+function solution(balls, share) {
+  let totalCombos = 0;
+
+  function recur(idx, currCombo) {
+    if (currCombo.length === share) {
+      totalCombos++;
+      return;
+    }
+    for (let i = idx; i < balls; i++) {
+      currCombo.push(i);
+      recur(i + 1, currCombo);
+      currCombo.pop();
+    }
+  }
+
+  recur(0, []);
+
+  return totalCombos;
+}
+
+const balls = 3;
+const share = 2;
+
+console.log(solution(balls, share));
