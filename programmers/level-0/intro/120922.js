93cf2e752b1ddfec74a0c7f5fc4845cd6fe15a70
HopeFullee <john_lee0607@naver.com>
2025-10-20 22:55:12 +0900

    chore: programmers 120922 solved

---
+++ programmers/level-0/intro/120922.js
@@ -0,0 +1,13 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/120922
+  코딩테스트 입문 - 종이 자르기
+*/
+
+function solution(M, N) {
+  return M * N - 1;
+}
+
+const M = 2;
+const N = 5;
+
+console.log(solution(M, N));
