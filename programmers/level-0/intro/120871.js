02c98b63f158b1eca32208f70be378ba4236bc70
HopeFullee <john_lee0607@naver.com>
2025-10-23 01:12:26 +0900

    chore: programmers 120871 solved

---
+++ programmers/level-0/intro/120871.js
@@ -0,0 +1,27 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/120871
+  코딩테스트 입문 - 저주의 숫자 3
+*/
+
+function solution(n) {
+  let fuckedUpNumber = 0;
+
+  for (let i = 0; i < n; i++) {
+    fuckedUpNumber++;
+
+    while (true) {
+      const hasThree = fuckedUpNumber.toString().includes("3");
+      const isMultiple = fuckedUpNumber % 3 === 0;
+
+      if (!hasThree && !isMultiple) break;
+
+      fuckedUpNumber++;
+    }
+  }
+
+  return fuckedUpNumber;
+}
+
+const n = 15;
+
+console.log(solution(n));
