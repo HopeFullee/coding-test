53595c2902cfaf9e6b5d555eea75cc3cefcda49a
HopeFullee <john_lee0607@naver.com>
2025-11-13 20:52:48 +0900

    chore: programmers 2742 solved

---
+++ baek-joon/tier-bronze/bronze-5/2742.js
@@ -0,0 +1,14 @@
+/*
+  https://www.acmicpc.net/problem/2742
+  bronze tier 5 - 기찍 N
+*/
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+function solution(input) {
+  return [...Array(Number(input))].reduce((a, _, idx) => {
+    return a + (Number(input) - idx) + "\n";
+  }, "");
+}
+
+console.log(solution(input));
