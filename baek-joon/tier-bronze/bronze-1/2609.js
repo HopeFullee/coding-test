87d698e59091302db7629866830d07f57914aaee
HopeFullee <john_lee0607@naver.com>
2025-11-14 21:44:40 +0900

    chore: baekjoon 2609 solved

---
+++ baek-joon/tier-bronze/bronze-1/2609.js
@@ -0,0 +1,39 @@
+/*
+  https://www.acmicpc.net/problem/1110
+  bronze tier 1 - 최대공약수와 최소공배수
+*/
+
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split(" ")
+//   .map(Number);
+
+const input = [24, 18];
+
+const Euclidean = (input) => {
+  let a = input[0];
+  let b = input[1];
+
+  while (a % b) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+
+  const GCM = b;
+  const LCM = (input[0] * input[1]) / b;
+
+  return { GCM, LCM };
+};
+
+function solution(input) {
+  input.sort((a, b) => b - a);
+
+  const { GCM, LCM } = Euclidean(input);
+
+  return `${GCM}\n` + `${LCM}`;
+}
+
+console.log(solution(input));
