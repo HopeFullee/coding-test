38c61f97da5b63adfe367d8080fe756d8428943a
HopeFullee <john_lee0607@naver.com>
2025-11-14 20:43:05 +0900

    chore: baekjoon 8958 solved

---
+++ baek-joon/tier-bronze/bronze-2/8958.js
@@ -0,0 +1,45 @@
+/*
+  https://www.acmicpc.net/problem/8958
+  bronze tier 2 - OX퀴즈
+*/
+
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split("\n");
+
+const input = [
+  "5",
+  "OOXXOXXOOO",
+  "OOXXOOXXOO",
+  "OXOXOXOXOXOXOX",
+  "OOOOOOOOOO",
+  "OOOOXOOOOXOOOOX",
+];
+
+input.shift();
+
+function solution(input) {
+  let ans = "";
+
+  input.forEach((tc) => {
+    let score = 0;
+    let combo = 0;
+
+    for (const ch of tc) {
+      if (ch === "X") {
+        combo = 0;
+        continue;
+      }
+      score += 1 + combo;
+      combo++;
+    }
+
+    ans += score.toString() + "\n";
+  });
+
+  return ans;
+}
+
+console.log(solution(input));
