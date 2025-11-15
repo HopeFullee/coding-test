41d8a29ccd26b53e84c59dbc10548d8ef27248f6
HopeFullee <john_lee0607@naver.com>
2025-11-15 23:33:27 +0900

    chore: baekjoon 2920 solved

---
+++ baek-joon/tier-bronze/bronze-2/2920.js
@@ -0,0 +1,24 @@
+/*
+  https://www.acmicpc.net/problem/2920
+  bronze tier 2 - 음계
+*/
+
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split(" ")
+//   .map(Number);
+
+const input = [8, 7, 6, 5, 4, 3, 2, 1];
+
+function solution(input) {
+  const asc = [...input].sort((a, b) => a - b);
+  const desc = [...input].sort((a, b) => b - a);
+
+  if (asc.join("") === input.join("")) return "ascending";
+  if (desc.join("") === input.join("")) return "descending";
+  return "mixed";
+}
+
+console.log(solution(input));
